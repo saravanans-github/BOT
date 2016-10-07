4735a2cf1df3b1d78bd0b3b9bbec734a4c443aa5
saravanans <saravanans.github@outlook.sg>
2016-10-08 04:19:03 +0800

    updated w/ grocerie convo

--- nayya.js
+++ nayya.js
@@ -27,9 +27,73 @@ function __startListening (err, data)
     } else
         this.token = data.Plaintext.toString('ascii');
 
-    bot = controller.spawn({
+   var bot = controller.spawn({
         token: this.token
-    }).startRTM();
+    });
+    
+    bot.startRTM();
+    
+    controller.on(['rtm_open'], function(bot, message) {
+            message = {channel:'D1M9KQWTG', user:'U1M8BBPD1', text:''};
+
+        bot.startConversation(message, function(err, convo) {
+            var askWhatToBuy = function(response, convo) {
+                convo.ask("what wld they be?", [
+                {
+                    pattern: new RegExp(/([0-9a-z]), ([0-9a-z])/i),
+                    callback: function(response,convo) {
+                        convo.say('Awesome... I\'ll remind you at 6pm today.');
+                        convo.say('ttyl');
+                        convo.next();
+                    }
+                },
+                {
+                    default: false,
+                    callback: function(reply, convo) {
+                        // As the question again.
+                        convo.repeat();
+                    }
+                }]);
+            };
+
+            convo.ask('hi @saravanans. Any groceries to buy today?',
+            [{
+                pattern: bot.utterances.yes,
+                callback: askWhatToBuy
+            },
+            {
+                pattern: bot.utterances.no,
+                callback: function(reply, convo) {
+                    convo.say('Np.');
+                    convo.say('Bye for nw.');
+                    convo.next();
+                }
+            },
+            {
+                default: true,
+                callback: function(reply, convo) {
+                    convo.repeat();
+                }
+            }]);
+
+
+            // TODO: Send the result of the conversation to SNS to process the next Rule
+            convo.on('end',function(convo) {
+                if (convo.status=='completed') {
+                    // do something useful with the users responses
+                    var res = convo.extractResponses();
+
+                    // reference a specific response by key
+                    var value  = convo.extractResponse('key');
+
+                    // ... do more stuff...
+
+                } else {
+                    // something happened that caused the conversation to stop prematurely
+                }
+            });
+        }); 
+    });
 }
 
 // Publish the message that Nayya recieves to the topic
@@ -61,20 +125,20 @@ function decryptKMS(callback) {
 
 function main()
 {
-    // Define what needs to be listened for and where to publish
-    controller.hears(['(.*)'], 'direct_message,direct_mention,mention', function(bot, message) 
-    {
-        // Construct the client.
-        snsClient = new AWS.SNS({credentials:AWS.credentials});
+    // // Define what needs to be listened for and where to publish
+    // controller.hears(['(.*)'], 'direct_message,direct_mention,mention', function(bot, message) 
+    // {
+    //     // Construct the client.
+    //     snsClient = new AWS.SNS({credentials:AWS.credentials});
 
-        console.log(message);
+    //     console.log(message);
 
-        // Get the list of topics
-        snsClient.listTopics(null, function (err,data) {
-            if(err) console.log(err, err.stack);
-            else   publishMsgToTopic({ text: message.text, channel: message.channel }, data.Topics[0].TopicArn);
-        }); 
-    });
+    //     // Get the list of topics
+    //     snsClient.listTopics(null, function (err,data) {
+    //         if(err) console.log(err, err.stack);
+    //         else   publishMsgToTopic({ text: message.text, channel: message.channel }, data.Topics[0].TopicArn);
+    //     }); 
+    // });
 
     decryptKMS(__startListening);
 }
