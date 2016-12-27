84bb51f774d830353d43aa2754929428c6afee7b
saravanans <saravanans.github@outlook.sg>
2016-12-27 23:32:47 +0800

    update endpoints for DynamoDB

--- dynamoDBScripts/queryTable.js
+++ dynamoDBScripts/queryTable.js
@@ -17,7 +17,7 @@ AWS.config.update({
     region:'ap-southeast-1'
 });
 
-var dynamodb = new AWS.DynamoDB({region_name:'ap-southeast-1', endpoint:"http://localhost:8000"});
+var dynamodb = new AWS.DynamoDB({region_name:'ap-southeast-1'/*, endpoint:"http://localhost:8000"*/});
 dynamodb.query(params, function(err, data){
   if (err) console.log(err, err.stack); // an error occurred
   else     console.log(data);           // successful response
--- ruleReminder.js
+++ ruleReminder.js
@@ -1,6 +1,6 @@
 var Promise = require('promise');
 var AWS = require('aws-sdk');
-var dynamodb = new AWS.DynamoDB({region_name:'ap-southeast-1', endpoint:"http://localhost:8000"});
+var dynamodb = new AWS.DynamoDB({region_name:'ap-southeast-1' /*, endpoint:"http://localhost:8000"*/});
 
 var RuleReminder =
 {
@@ -17,8 +17,6 @@ var RuleReminder =
                 ":active": {"BOOL": byActive}
             },
         };
-
-        console.log(params);
 
         var request = dynamodb.query(params);
         var promise = request.promise();
