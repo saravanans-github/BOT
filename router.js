ef40c1e266fc32500471c7ddbd412db6aa54d85b
saravanans <saravanans.github@outlook.sg>
2016-12-28 00:48:47 +0800

    optmized count

--- router.js
+++ router.js
@@ -61,7 +61,7 @@ function __getReminders (req, res, next){
             reminders[i] = reminder;
         }
 
-        var response = { channel: req.query.byOwnerId, count: reminders.length, items: reminders};
+        var response = { channel: req.query.byOwnerId, count: data.count, items: reminders};
 
 
         res.set('Content-Type', 'application/json');
