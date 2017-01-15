c436824b15149977c8af8b8c40283b066f010b15
saravanans <saravanans.github@outlook.sg>
2017-01-15 18:52:53 +0800

    same as prev commit

--- nayya.js
+++ nayya.js
@@ -3,7 +3,7 @@ var Botkit = require('botkit');
 Nayya.DONE = ['(done)','(paid)','(settled)','(setled)'];
 Nayya.MOTIVATION = ['(worse)', '\bbad\W+(?:\w+\W+){1,6}?work\b', '\bshit\W+(?:\w+\W+){1,6}?work\b', '\bbad\W+(?:\w+\W+){1,6}?finance\b'];
 
-Nayya.MOTIVATIONS = ['Things hv been worse in the past... Cheer up.', 
+Nayya.MOTIVATIONS = ['Don\t worry. Things hv been worse in the past... Cheer up.', 
                      'The night is darkest before dawn... Good times are comming.',
                      'Great things do not come easy... hold on tight.',
                      'Pain is an investment for a greater good.']
